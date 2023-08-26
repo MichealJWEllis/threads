c85a4de0039ee3a938a9226977ff2f109b80ccfb
michealjwellis <mellis.thedev@gmail.com>
2023-08-26 19:18:36 -0400

    working from pc

--- components/shared/Bottombar.tsx
+++ components/shared/Bottombar.tsx
@@ -1,9 +1,7 @@
 "use client";
-
 import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-
 import { sidebarLinks } from "@/constants";
 
 function Bottombar() {
